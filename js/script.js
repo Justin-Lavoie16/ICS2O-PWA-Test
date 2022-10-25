677112fddedf609f24acf0c08121f6f8b90ebc07
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-10-25 23:52:09 +0000

    123

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 ;("use strict")
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/", {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
     scope: "/ICS2O-PWA-Test/",
   })
 }
