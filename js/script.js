af84f1ec1a0223280945a25a8caafd71417a7b3c
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-10-26 00:05:00 +0000

    changed a few things

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 ;("use strict")
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", 
+{
     scope: "/ICS2O-PWA-Test/",
   })
 }
